e8427fe084351f7a6afdc2aac37930844447fdb5
Neil Grogan <neil@grogan.org>
2024-10-12 22:52:17 +0100

    try devshell

---
+++ archetypes/post.md
@@ -0,0 +1,10 @@
+---
+title: "{{ replaceRE `[-_]` " " (replaceRE `^([0-9]{4}-[0-9]{2}-[0-9]{2}-)?` "" .Name) | title }}"
+date: {{ .Date }}
+draft: true # TODO: un-draft to publish this
+
+# categories: ["python", "test"]
+---
+
+<!--more-->
+
--- flake.nix
+++ flake.nix
@@ -1,26 +1,84 @@
 {
-  description = "Neil Grogan Hugo Website";
+  description = "my blog";
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      systems = [
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        final,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+
+        devshells.default = {
+          commands = [
+            {package = pkgs.hugo;}
+            {
+              name = "dev";
+              help = "Run the hugo server";
+              command = ''
+                (sleep 1 ; open http://localhost:1313/) &
+                exec hugo server --noHTTPCache --buildDrafts --buildFuture \"$@\"
+              '';
+            }
+            {
+              name = "new-post";
+              help = "Create a new post with '[plain-title]'";
+              command = ''
+                set -eu -x
+                date="$(date -Idate)"
+                year="$(date +%Y)"
+                title_plain="$1"; shift
+                title="$(echo "$title_plain" | tr ' A-Z' '-a-z')"
+
+                hugo new content -k post "$@" content/post/"$year"/"$date"-"$title".md
+                "$EDITOR" content/post/"$year"/"$date"-"$title".md
+              '';
+            }
+          ];
+          packages = with pkgs; [go git];
+        };
+
+        checks.versions = inputs.nix-flake-tests.lib.check {
+          inherit pkgs;
+          tests = {
+            testMatch = let
+              netlifyVersion = (builtins.fromTOML (builtins.readFile ./netlify.toml)).build.environment.HUGO_VERSION;
+              pkgsVersion = pkgs.hugo.version;
+            in {
+              expected = netlifyVersion;
+              expr = pkgsVersion;
+            };
+          };
+        };
+      };
+    };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nix-flake-tests.url = "github:antifuchs/nix-flake-tests";
   };
-
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in
-      with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [
-            hugo
-            ncftp
-            rsync
-          ];
-        };
-      });
 }
